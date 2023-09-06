2c041a38c70e3b5be913dd7de7c99046f38d706c
Neto test <demon_4@mail.ru>
2023-09-06 20:50:12 +0300

    add corrected code jococo

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,16 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxBetween() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 52, 3, 8, 6, 11, 11, 12};
+        long expected = 52;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
